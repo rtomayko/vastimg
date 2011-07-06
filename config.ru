50b1b0325d2a4e7fced6ebd5df7f295c2664043c
Ryan Tomayko <rtomayko@gmail.com>
2011-07-06 00:27:09 -0700

    classes

--- config.ru
+++ config.ru
@@ -1,28 +1,5 @@
-history = ['http://thechive.files.wordpress.com/2010/11/oprah-bees.gif']
+root = File.dirname(__FILE__)
+$:.unshift(root)
 
-def scrub(str)
-  str.to_s.strip.gsub(/[\r\n\t\0]/, '').gsub('%0D', '')
-end
-
-run lambda { |env|
-  url = scrub(env['QUERY_STRING'])
-  case url
-  when /^(https?|data):/
-    history << url if !history.include?(url)
-    if env['HTTP_USER_AGENT'] =~ /Propane/
-      [302, {'Location' => url}, []]
-    else
-      [
-        200,
-        {
-          'Content-Type'  => 'text/html',
-          'Cache-Control' => 'public, max-age=0'
-        },
-        File.open('index.html', 'rb')
-      ]
-    end
-  else
-    history << history.shift
-    [302, {'Location' => "/?#{history.first}"}, []]
-  end
-}
+require 'vastimg'
+run Vastimg.new
---
+++ vastimg.rb
@@ -0,0 +1,32 @@
+class Vastimg
+  def initialize
+    @history = ['http://thechive.files.wordpress.com/2010/11/oprah-bees.gif']
+  end
+
+  def call(env)
+    url = scrub(env['QUERY_STRING'])
+    case url
+    when /^(https?|data):/
+      @history << url if !@history.include?(url)
+      if env['HTTP_USER_AGENT'] =~ /Propane/
+        [302, {'Location' => url}, []]
+      else
+        [
+          200,
+          {
+            'Content-Type'  => 'text/html',
+            'Cache-Control' => 'public, max-age=0'
+          },
+          File.open('index.html', 'rb')
+        ]
+      end
+    else
+      @history << @history.shift
+      [302, {'Location' => "/?#{@history.first}"}, []]
+    end
+  end
+
+  def scrub(str)
+    str.to_s.strip.gsub(/[\r\n\t\0]/, '').gsub('%0D', '')
+  end
+end
